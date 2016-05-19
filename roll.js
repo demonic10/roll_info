3657e77a507115589b1f8d2a7e51dca31dbeff30
Mark <oper29a@gmail.com>
2016-05-19 09:35:59 +0500

    Done some rework.

--- roll.js
+++ roll.js
@@ -1,68 +1,54 @@
 $(document).ready(function () {
 
 
-function roll_sum(dice){
-    var sum = 0;
-    for (var i = 0; i < 1000; i++) {
-        sum += Math.floor((Math.random() * dice) + 1);
+var roll_sum = function(dice, count = 1){
+    var dice = dice;
+    var count = count;
+    return function(){
+      var sum = 0;
+      for (var i = 0; i < 1000; i++) {
+        for(var n = 0; n < count; n++) {
+          sum += Math.floor((Math.random() * dice) + 1);
+        }
+      }
+      return sum;
     }
-    return sum;
 }
 
-var dataSource1 = [
-    { breed: "Experiment_1", number: roll_sum(12) },
-    { breed: "Experiment_2", number: roll_sum(12) },
-    { breed: "Experiment_3", number: roll_sum(12) },
-    { breed: "Experiment_4", number: roll_sum(12) },
-    { breed: "Experiment_5", number: roll_sum(12) },
-    { breed: "Experiment_6", number: roll_sum(12) },
-    { breed: "Experiment_7", number: roll_sum(12) },
-    { breed: "Experiment_8", number: roll_sum(12) },
-    { breed: "Experiment_9", number: roll_sum(12) },
-    { breed: "Experiment_10", number: roll_sum(12) }
-],
-    dataSource2 = [
-    { breed: "Experiment_1", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_2", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_3", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_4", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_5", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_6", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_7", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_8", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_9", number: (roll_sum(6)+roll_sum(6)) },
-    { breed: "Experiment_10", number: (roll_sum(6)+roll_sum(6)) }
-]; 
 
+var experiment = function($func, count = 10) {
+  var data = [];
+  for(var i = 1; i <= count; i++) {
+    data.push({
+      breed: "Experiment_" + i,
+      number: $func()
+    });
+  }
+  return data;
+}
 
-$("#chartContainer1").dxChart({
-    dataSource: dataSource1,
-    title: "Броски 1 d12",
-    series: {
-        type: "bar",
-        argumentField: "breed",
-        valueField: "number",
-        tagField: "photo",
-        hoverStyle: {
-            hatching: "right"
-        }
-    },
-    legend: { visible: false },
-});
+var showExp = function(selector, title, dataSource) {
+  $(selector).dxChart({
+      dataSource: dataSource,
+      title: title,
+      series: {
+          type: "bar",
+          argumentField: "breed",
+          valueField: "number",
+          tagField: "photo",
+          hoverStyle: {
+              hatching: "right"
+          }
+      },
+      legend: { visible: false },
+  });
+}
 
-$("#chartContainer2").dxChart({
-    dataSource: dataSource2,
-    title: "Броски 2 d6",
-    series: {
-        type: "bar",
-        argumentField: "breed",
-        valueField: "number",
-        tagField: "photo",
-        hoverStyle: {
-            hatching: "right"
-        }
-    },
-    legend: { visible: false },
-});
+const count = 100;
+showExp("#chartContainer1", "Броски d12", experiment(roll_sum(12), count));
+showExp("#chartContainer2", "Броски 2d6", experiment(roll_sum(6, 2), count));
+showExp("#chartContainer3", "Броски 3d4", experiment(roll_sum(4, 3), count));
+showExp("#chartContainer4", "Броски 4d3", experiment(roll_sum(3, 4), count));
+showExp("#chartContainer5", "Броски 6d2", experiment(roll_sum(2, 6), count));
 
 });
